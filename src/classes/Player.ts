cb79b8c62e55ebaf6b09a99b1e5bbf39cb21ef63
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-09-03 14:53:34 -0400

    feat: complete `Player` class + tests

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,7 +1,36 @@
+import { Fleet } from './Fleet'
+import type { Ship } from './Ship'
+
 export class Player {
   private id: string
+  private fleet: Fleet
 
   constructor() {
     this.id = crypto.randomUUID()
+    this.fleet = new Fleet()
+  }
+
+  public getId(): string {
+    return this.id
+  }
+
+  public getFleet(): Fleet {
+    return this.fleet
+  }
+
+  public addShip(ship: Ship): void {
+    this.fleet.addShip(ship)
+  }
+
+  public removeShip(ship: Ship): void {
+    this.fleet.removeShip(ship)
+  }
+
+  public isDefeated(): boolean {
+    return this.fleet.isDestroyed()
+  }
+
+  public reset(): void {
+    this.fleet = new Fleet()
   }
 }
