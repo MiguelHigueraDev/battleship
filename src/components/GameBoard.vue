d07fb5415435edaff5aed5f3f9a3cae0b4cf7e15
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-09-07 18:04:45 -0400

    feat: add basic prototype UI that allows for placement and removal of ships

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,152 +1,50 @@
 <script setup lang="ts">
+import { usePlayerStore } from '@/stores/playerStore'
+import { storeToRefs } from 'pinia'
+
+const playerStore = usePlayerStore()
+const { player } = storeToRefs(playerStore)
+const { placeOrRemoveShip } = playerStore
 </script>
 
 <template>
-    <div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-    </div>
+  <div class="row" :key="rowIndex" v-for="(row, rowIndex) in player.getBoard().getPlacedShips()">
+    <button
+      class="cell"
+      :class="{ ship: shipCell != null }"
+      :key="cellIndex"
+      v-for="(shipCell, cellIndex) in row"
+      @click="() => placeOrRemoveShip({ x: cellIndex, y: rowIndex })"
+    >{{ rowIndex}}, {{cellIndex }}</button>
+  </div>
 </template>
 
 <style scoped lang="css">
 .row {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
 }
 
 .cell {
-    width: 50px;
-    height: 50px;
-    border: 1px solid black;
-    margin: 0.5px;
-    background-color: white;
-    cursor: pointer;
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+  margin: 0.5px;
+  background-color: white;
+  cursor: pointer;
 }
 
 .cell:hover {
-    background-color: lightgray;
+  background-color: lightgray;
 }
 
 .cell:active {
-    background-color: gray;
+  background-color: gray;
+}
+
+.ship {
+  background-color: blue;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import GameBoard from '@/components/GameBoard.vue'
+import ShipSelector from '@/components/ShipSelector.vue'
 </script>
 
 <template>
   <main>
     <GameBoard></GameBoard>
+    <ShipSelector></ShipSelector>
   </main>
 </template>
